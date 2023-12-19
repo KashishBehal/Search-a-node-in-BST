e93a7026bc02c31e06792e3eecd3ba09b25559dc
Kashish Behal <113112253+KashishBehal@users.noreply.github.com>
2023-12-19 19:16:17 +0530

    Create Search a node in BST.cpp

---
+++ Search a node in BST.cpp
@@ -0,0 +1,14 @@
+bool search(Node* root, int x) {
+    if(root==NULL){
+        return false;
+    }
+    if(root->data == x){
+        return true;
+    }
+    if(root->data> x){
+        search(root->left , x);
+    }
+    else{
+        search(root->right , x);
+    }
+}
